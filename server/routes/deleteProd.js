b2e23baf28affc24ac59f25debb7d9bd9d9b6254
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-09-19 04:48:37 +1000

    added delete product code

--- server/routes/deleteProd.js
+++ server/routes/deleteProd.js
@@ -1,3 +1,17 @@
-module.exports = function(db, app) {
-    
+module.exports = function(db, app, ObjectID) {
+    app.post('/prod/delete', function(req, res){
+        if (!req.body) {
+            return res.sendStatus(400);
+        }
+        productID = req.body.productid;
+        var objectid = new ObjectID(productID);
+        const collection = db.collection('products');
+        // delete item with the id
+        collection.deleteOne({_id:objectid}, (err, docs) => {
+            // send the new array of users
+            collection.find({}).toArray((err, data) => {
+                res.send(data);
+            });
+        });
+    });
 }
