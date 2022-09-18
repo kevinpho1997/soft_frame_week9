c5687debc0add7d7f003df9fc72aa40b35b5d8c9
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-09-19 03:57:42 +1000

    added add product code

---
+++ server/routes/addProd.js
@@ -0,0 +1,22 @@
+module.exports = function(db,app){
+    app.post('/prod/add', function(req,res){
+        if (!req.body) {
+            return res.sendStatus(400);
+        }
+        product = req.body;
+        const collection = db.collection('products');
+        collection.find({'id':product.id}.count((err, count)=>{
+            if (count == 0) {
+                collection.inserOne(product, (err, dbRes)=>{
+                    if (err) throw err;
+                    // sends no. of products inserted to client w/ no error msg
+                    let num = dbRes.insertedCount;
+                    res.send({'num':num, err:null});
+                });
+            } else {
+                // sends error if item already exists
+                res.send({num:0, err:"Duplicate item"})
+            }
+        }));
+    });
+}
