3dcc36ad11d059db80dd3dc0bf897757d13ccc3a
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-09-22 01:39:55 +1000

    added product service code

--- MyProducts/src/app/services/product-data.service.ts
+++ MyProducts/src/app/services/product-data.service.ts
@@ -7,33 +7,35 @@ import { Product } from '../models/product';
 })
 export class ProductDataService {
 
+  server = "http://localhost:3000";
+
   constructor(private http: HttpClient) { }
 
   addProduct(product: Product) {
-
+    return this.http.post<any>(`${this.server}/prod/add`, product);
   }
 
   getProductList() {
-
+    return this.http.get<any>(`${this.server}/prod/getList`);
   }
 
-  getItem(productID: any) {
+  // getItem(productID: any) {
 
-  }
+  // }
 
   updateProduct(product: Product) {
-
+    return this.http.post<any>(`${this.server}/prod/update`, product);
   }
 
   deleteProduct(productID: any) {
-    
+    return this.http.post<any>(`${this.server}/prod/delete`, {'productid': productID});
   }
 
   checkValidId(productID: any){
-
+    return this.http.post<any>(`${this.server}/validateID`, {'id': productID});
   }
 
   getProductCount() {
-    
+    return this.http.get<any>(`${this.server}/prod/count`);
   }
 }
