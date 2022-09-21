df815d9e2d9beca82d4428ec72aceb6ab5a23f65
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-09-22 00:51:18 +1000

    add bootstrap added nav

--- MyProducts/src/app/app-routing.module.ts
+++ MyProducts/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GetAllProductsComponent } from './get-all-products/get-all-products.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'products', component: GetAllProductsComponent},{path: 'update', component: UpdateProductComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
