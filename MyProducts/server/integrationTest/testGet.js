c150ca907ea7cc12511357d89d3f9dda2ccbede4
Kevin Pho <88666647+kevinpho1997@users.noreply.github.com>
2022-09-29 19:41:24 +1000

    make tests getProdList and addProd pass

--- MyProducts/server/integrationTest/testGet.js
+++ MyProducts/server/integrationTest/testGet.js
@@ -1,30 +1,46 @@
 var assert = require('assert');
 var app = require('../server.js');
-
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-
+// const { get} = require('http');
 chai.use(chaiHttp);
+let should = chai.should();
 
 describe('Server integration test', function() {
     // The function passed to before() is called before running the test cases.
     before(function() {
         console.log("before test");
     });
-
     // The function passed to after() is called after running the test cases.
     after(function() {
         console.log("after test");
     });
-
     describe('/getProdList', () => {
         it('it should GET all products', (done) => {
-            chai.request(app)
-                .get('./routes/getProdList')
+            chai.request(app).get('/prod/getList')
+                //.get('./routes/getProdList')
                 .end((err, res) => {
                     // should.exist(res.body);
                     res.should.have.status(200);
                     res.body.should.be.a('array');
+                    done();
+                });
+        });
+    });
+    describe('/addProd', () => {
+        it('it should insert a doc (product)', (done) => {
+            chai.request(app)
+                .post('/prod/add').type('form').send({ 'id': 25, 'name': "iPhone", 'description': 'My description', 'price': 1500, 'units': 20 })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    // console.log("res.body: ", res.body);
+                    // res.body.should.have.property('num').to.not.equal(0);
+                    res.body.should.have.property('err').to.be.null;
+                    // res.body.should.have.property('id');
+                    // res.body.should.have.property('name');
+                    // res.body.should.have.property('description');
+                    // res.body.should.have.property('price');
+                    // res.body.should.have.property('units');
                     done();
                 });
         });
